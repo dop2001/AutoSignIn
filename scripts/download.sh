bff98327f2d5a6830fd8f606975e577ec0b443f4
zoujingchen <1213607658@qq.com>
2024-01-12 21:12:16 +0800

    1

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
+
+# git clone repository from GitHub
+git clone git@github.com:dop2001/Action.git
+cd Action
+
+# get current path
 CurrentPath=$(pwd)
+
+# Download file path
+EdgeURL=https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_120.0.2210.133-1_amd64.deb?brand=M102
+DriverURL=https://msedgedriver.azureedge.net/120.0.2210.133/edgedriver_linux64.zip
+
+# download and install Edge web browser
+wget -c $EdgeURL -O Edge.deb
+sudo dpkg -i Edge.deb
+
+if [ $? -ne 0 ]; then
+    echo "Edge install fail!"
+    exit 1
+fi
+
+# download and install Edge driver
+
+
+
+echo $CurrentPath
